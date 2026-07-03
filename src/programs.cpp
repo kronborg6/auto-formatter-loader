83bf15c7496b30abe4eeea758087ee1f56937fbe
Kronborg <mkronborg7@gmail.com>
2026-07-03 18:17:43 +0200

    need that on wsl debian don't no why

--- src/programs.cpp
+++ src/programs.cpp
@@ -1,9 +1,11 @@
 #include "formatters/templateLoader.hpp"
 #include "progams.hpp"
+#include <algorithm>
+#include <cctype>
 #include <filesystem>
 
-bool Programs::CreateNewFormatter(const std::filesystem::directory_entry& entry,
-                                  const option::TemplateLoader& templates) {
+bool Programs::CreateNewFormatter(const std::filesystem::directory_entry &entry,
+                                  const option::TemplateLoader &templates) {
   if (!entry.is_directory())
     return false;
 
