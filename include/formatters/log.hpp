c24d617aa7ab67bff3e8ad7cf28fe0e7ce3a319c
Mikkel Kronborg <mkronborg7@gmail.com>
2026-07-14 19:16:30 +0200

    start on log even when this branch is for another thing

--- include/formatters/log.hpp
+++ include/formatters/log.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <ostream>
+#include <string_view>
 
 namespace Config {
   class Log {
     public:
       Log();
       // explicit Log(std::ostream& output) : output_(output) {};
-      void log(std::ostream& output, std::string text);
-      void log(std::string text);
+      void write(std::ostream& output, std::string_view text);
+      void write(std::string_view text);
 
     private:
       std::ostream& output_;
--- src/log.cpp
+++ src/log.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
+#include <string_view>
 
 namespace {
   std::ostream& getLogOutput() {
@@ -21,5 +22,9 @@ namespace {
 
 namespace Config {
   Log::Log() : output_(getLogOutput()) {
+    output_ << "LOG START AT XXXXXXX" << "\n";
+  }
+  void Log::write(std::string_view text) {
+    output_ << text << "\n";
   }
 } // namespace Config
