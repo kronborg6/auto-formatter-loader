49562cd58db26860e92ec4421bb9768976a7c72f
Mikkel Kronborg <mkronborg7@gmail.com>
2026-07-19 00:24:39 +0200

    more about loging

--- src/log.cpp
+++ src/log.cpp
@@ -1,15 +1,7 @@
 #include "formatters/log.hpp"
-#include "formatters/config.hpp"
-#include <algorithm>
-#include <chrono>
-#include <ctime>
-#include <fstream>
-#include <iomanip>
+#include "helper.hpp"
 #include <iostream>
 #include <ostream>
-#include <sstream>
-#include <stdexcept>
-#include <string>
 #include <string_view>
 
 namespace {
@@ -35,36 +27,11 @@ namespace Config {
     output_ << "LOG START AT XXXXXXX" << "\n";
   }
   Log::Log(std::ostream& output) : output_(output) {
-    auto time = getTimeNow();
+    auto time = Helper::getTimeNow();
     output_ << "[started at: " << time << "]\n";
   }
   void Log::write(std::string_view text) {
     output_ << text << "\n";
-  }
-
-  std::time_t Log::getTimeNowUtc() {
-    const auto now = std::chrono::system_clock::now();
-    return std::chrono::system_clock::to_time_t(now);
-  }
-
-  std::string Log::getTimeNow(bool withDate) {
-    const std::time_t now = getTimeNowUtc();
-    std::tm utcTime{};
-
-#ifdef _WIN32
-    gmtime_s(&utcTime, &now);
-#else
-    gmtime_r(&now, &utcTime);
-#endif
-
-    std::ostringstream output;
-    if (withDate) {
-      output << std::put_time(&utcTime, "%Y-%m-%d %H:%M:%S UTC");
-    } else {
-      output << std::put_time(&utcTime, "%H:%M:%S UTC");
-    }
-
-    return output.str();
   }
 
 } // namespace Config
