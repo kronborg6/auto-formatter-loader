62ae9ab73f883bd1da49e6c65d9c7bf238103b9f
Mikkel Kronborg <mkronborg7@gmail.com>
2026-07-19 22:12:23 +0200

    learned to type

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,11 @@
 #include "helper.hpp"
 #include "iostream"
 #include "progams.hpp"
-#include "yaml-cpp/node/parse.h"
 #include <cctype>
 #include <chrono>
-#include <cmath>
 #include <cstddef>
 #include <filesystem>
 #include <format>
-#include <fstream>
-#include <ostream>
-#include <ranges>
 #include <set>
 #include <string>
 #include <sys/inotify.h>
